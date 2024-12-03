23f4f8eb6c3fb19b31cd4d2d04f5ebc2d9b91266
Bach Nguyen <bachnv@vng.com.vn>
2024-12-03 16:30:59 +0300

    add data base

--- UrlShortaner.API/Program.cs
+++ UrlShortaner.API/Program.cs
@@ -1,5 +1,6 @@
 using CleanArchitecture.Web.Infrastructure;
 using UrlShortaner.API;
+using UrlShortaner.Infratructure;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -7,6 +8,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddInfrastructureServices(builder.Configuration);
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddApplicationServices();
 builder.Services.AddHealthChecks();
