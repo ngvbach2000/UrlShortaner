9f51ffdef14e8487cc2ec44b30a171e4ecfe4780
Bach Nguyen <bachnv@vng.com.vn>
2024-12-03 15:18:38 +0300

    add swagger

--- UrlShortaner.API/Program.cs
+++ UrlShortaner.API/Program.cs
@@ -8,18 +8,19 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
 builder.Services.AddApplicationServices();
 builder.Services.AddHealthChecks();
+builder.Services.AddOpenApiDocument();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+app.UseOpenApi();
+app.UseSwaggerUi(settings =>
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+    settings.Path = "/api";
+    // settings.DocumentPath = "/api/specification.json";
+});
 
 app.UseHttpsRedirection();
 app.UseHealthChecks("/health");
